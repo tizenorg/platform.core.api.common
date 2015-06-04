1fac8bc8e298f0805c0531b49019d051e7ed2934
ByungWoo Lee <bw1212.lee@samsung.com>
2015-06-04 13:06:38 +0900

    Fix build break
    Change-Id: Ia04a26250948aa6e67b0e389f534f910cbde96f4

--- include/tizen_error.h
+++ include/tizen_error.h
@@ -352,7 +352,7 @@ typedef enum
     TIZEN_ERROR_TIMED_OUT, /**< Time out */
     TIZEN_ERROR_NOT_SUPPORTED, /**< Not supported */
     TIZEN_ERROR_USER_NOT_CONSENTED, /**< Not Consented */
-    TIZEN_ERROR_END_OF_COLLECTION
+    TIZEN_ERROR_END_OF_COLLECTION,
     TIZEN_ERROR_NOT_SUPPORT_API = TIZEN_ERROR_NOT_SUPPORTED /**< TIZEN_ERROR_NOT_SUPPORT_API is deprecated */
 } tizen_error_e;
 
