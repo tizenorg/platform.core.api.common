439e4e485bf239d842b329a3e7cbbddde69c99d6
ByungWoo Lee <bw1212.lee@samsung.com>
2016-05-13 14:06:29 +0900

    Add error message for capi-media-tool into tizen_error_private.h
    Change-Id: Ic2326384e4ee6b5e8092c874e1edfb253e56f5b5

--- tool/update_msg_header.sh
+++ tool/update_msg_header.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_PATH=`dirname $(readlink -f $0)`
 
-python $SCRIPT_PATH/make_msg_header.py
 echo ""
 echo "python $SCRIPT_PATH/make_msg_header.py"
+python $SCRIPT_PATH/make_msg_header.py
 
-python $SCRIPT_PATH/find_differences.py $SCRIPT_PATH/../include/private/tizen_error_private.h $SCRIPT_PATH/tizen_error_private.h
 echo ""
 echo "python $SCRIPT_PATH/find_differences.py $SCRIPT_PATH/../include/private/tizen_error_private.h $SCRIPT_PATH/tizen_error_private.h"
+python $SCRIPT_PATH/find_differences.py $SCRIPT_PATH/../include/private/tizen_error_private.h $SCRIPT_PATH/tizen_error_private.h
 
-mv $SCRIPT_PATH/tizen_error_private.h $SCRIPT_PATH/../include/private/tizen_error_private.h
 echo ""
 echo "mv $SCRIPT_PATH/tizen_error_private.h $SCRIPT_PATH/../include/private/tizen_error_private.h"
+mv $SCRIPT_PATH/tizen_error_private.h $SCRIPT_PATH/../include/private/tizen_error_private.h
